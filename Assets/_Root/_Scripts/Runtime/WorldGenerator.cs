96c0fcfa93b2320495d33372de906a12cb1168f3
Piotr Piwoni <piotrpiwonibiz@gmail.com>
2025-12-28 02:13:21 +0000

    Created a Basic World Generator.
    - Created a script called WorldGenerator that on start up places tiles within defined world size.

---
+++ Assets/_Root/_Scripts/Runtime/WorldGenerator.cs
@@ -0,0 +1,39 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace PixelCiv
+{
+public class WorldGenerator : MonoBehaviour
+{
+	[SerializeField, MinValue(1),]
+	private Vector2Int _WorldSize = new(10, 15);
+	[SerializeField]
+	private Tilemap _GroundTilemap;
+	[SerializeField]
+	private TileBase _GroundTile;
+
+
+	private void Start()
+	{
+		GenerateWorld();
+	}
+
+	private void GenerateWorld()
+	{
+		_GroundTilemap.ClearAllTiles();
+
+		BoundsInt bounds = new(0, 0, 0, _WorldSize.x, _WorldSize.y, 1);
+		var tiles = new TileBase[_WorldSize.x * _WorldSize.y];
+
+		for (var y = 0; y < _WorldSize.y; y++)
+		for (var x = 0; x < _WorldSize.x; x++)
+		{
+			int index = y * _WorldSize.x + x;
+			tiles[index] = _GroundTile;
+		}
+
+		_GroundTilemap.SetTilesBlock(bounds, tiles);
+	}
+}
+}
