c7d8287cd55806eb94fd5a3afbdec7a11b411366
Piotr Piwoni <piotrpiwonibiz@gmail.com>
2026-01-02 17:59:36 +0000

    Formatted the UnitManager Class.
    - Added function descriptions.
    - Added comments to code.
    - Added an overload to the Move().

--- Assets/_Root/_Scripts/Runtime/Managers/UnitManager.cs
+++ Assets/_Root/_Scripts/Runtime/Managers/UnitManager.cs
@@ -15,6 +15,12 @@ public class UnitManager : Singleton<UnitManager>
 	private readonly List<Unit> _Units = new();
 
 
+	/// <summary>
+	///     Create a new Unit object.
+	/// </summary>
+	/// <param name="position">The Unit's spawning position in offset coordinates.</param>
+	/// <param name="colour">Optional colour to set the sprite tint to.</param>
+	/// <returns></returns>
 	public Unit CreateUnit(Vector3Int position, Color? colour = null)
 	{
 		if (_Units.Any(unit => unit.CellPosition == position))
@@ -35,6 +41,11 @@ public class UnitManager : Singleton<UnitManager>
 		return CreateUnit(Hex.AxialToOffset(axial), colour);
 	}
 
+	/// <summary>
+	///     Move a desired Unit towards a specified cell.
+	/// </summary>
+	/// <param name="id">The GUID of the Unit to be moved.</param>
+	/// <param name="targetCell">The target cell's position in offset coordinates.</param>
 	public void Move(Guid id, Vector3Int targetCell)
 	{
 		Unit unit = _Units.Find(u => u.ID == id);
@@ -56,6 +67,11 @@ public class UnitManager : Singleton<UnitManager>
 		unit.OnMovementCompleted += CompleteMoveOrder;
 	}
 
+	public void Move(Guid id, Vector2Int targetCell)
+	{
+		Move(id, Hex.AxialToOffset(targetCell));
+	}
+
 	private void CompleteMoveOrder(Guid id)
 	{
 		Unit unit = _Units.Find(u => u.ID == id);
@@ -68,6 +84,12 @@ public class UnitManager : Singleton<UnitManager>
 		_MoveOrders.Remove(id);
 	}
 
+	/// <summary>
+	///     A* pathing calculation to a desired position.
+	/// </summary>
+	/// <param name="start">The starting cell position, in axial coordinates.</param>
+	/// <param name="goal">The destination cell position, in axial coordinates.</param>
+	/// <returns></returns>
 	private List<Vector3Int> FindPath(Vector2Int start, Vector2Int goal)
 	{
 		var frontier = new List<Vector2Int> { start, };
@@ -76,8 +98,10 @@ public class UnitManager : Singleton<UnitManager>
 		var totalCostToGoal = new Dictionary<Vector2Int, int>
 				{ [start] = movementCost[start] + Hex.Distance(start, goal), };
 
+		// Loop until the no more tiles are left to check.
 		while (frontier.Count > 0)
 		{
+			// Get the current tile based on the shorest distance to goal.
 			Vector2Int currentTile = frontier
 									 .OrderBy(n => totalCostToGoal.GetValueOrDefault(
 													  n, int.MaxValue)).First();
@@ -86,19 +110,24 @@ public class UnitManager : Singleton<UnitManager>
 			if (currentTile == goal)
 				return ReconstructPath(traveledPath, currentTile);
 
+			// Check each neighbour of the current tile.
 			foreach (Vector2Int neighbour in Hex.GetNeighbours(currentTile))
 			{
+				// Ignore invalid tiles.
 				Hex neighbourTile = GameManager.Instance.HexMap.Find(neighbour);
 				if (neighbourTile == null || neighbourTile.UnitID != Guid.Empty ||
 					!neighbourTile.Visuals)
 					continue;
 
+				// Calculate the assumed movement cost from the current tile to it's neighbour.
+				// Skip neighbour if we already know its movement cost.
 				const int TILE_MOVE_COST = 1;
 				int assumedMoveCost = movementCost[currentTile] + TILE_MOVE_COST;
 				if (movementCost.ContainsKey(neighbour) &&
 					assumedMoveCost >= movementCost[neighbour])
 					continue;
 
+				// Add to path.
 				traveledPath[neighbour] = currentTile;
 				movementCost[neighbour] = assumedMoveCost;
 				totalCostToGoal[neighbour] =
@@ -108,6 +137,8 @@ public class UnitManager : Singleton<UnitManager>
 			}
 		}
 
+		// In case no valid path was found.
+		Debug.LogWarning("No valid path was found!");
 		return new List<Vector3Int>();
 	}
 
@@ -115,6 +146,7 @@ public class UnitManager : Singleton<UnitManager>
 			Vector2Int current)
 	{
 		var path = new List<Vector2Int>();
+		// Continue until the current tile is equal to itself.
 		while (true)
 		{
 			path.Add(current);
